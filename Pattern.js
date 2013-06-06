55ac6c698757a60b72e550b5bbbcc23b55619343
Rivfader <huicho.argumedo@gmail.com>
2013-06-06 09:29:12 -0500

    added colors, camera rotation, fixed speed, addded fixed size

--- Pattern.js
+++
@@ -1,9 +0,0 @@
-function Pattern(scene) {
-	this.scene = scene;
-	Tile.side = 32;
-	this.speed = 2;
-	this.geometry = new THREE.CubeGeometry(Tile.side,Tile.side,Tile.side,1,1,1);
-	//this.material = new THREE.MeshNormalMaterial();
-	this.material = new THREE.MeshBasicMaterial({color:0x00BB00});
-	this.mesh = new THREE.Mesh(this.geometry,this.material);
-}
